eb3a2dd298edaef57308423f1d7c949134e22b20
paulsunny90 <paulsunny87267@gmail.com>
2025-10-16 17:57:27 +0530

    js functions

---
+++ index.js
@@ -0,0 +1,60 @@
+// //recursev function
+// function factorial(n) {
+//   if (n === 0) {    
+//     return 1;
+//     } else {        
+//     return n * factorial(n - 1);
+//     }
+// }
+
+
+// console.log(factorial(5)); 
+// console.log(factorial(0)); 
+
+
+//infinite recursion
+//  let i=0
+// function asdd() {
+//     console.log("asdd" +i);
+//     i++;
+//     if(i<5)
+//     asdd();
+// }
+
+// asdd();
+// //factorial
+// function asd(n){
+//     if(n==0){
+//         return 1;
+//     }
+//         return n * asd(n-1);
+    
+
+    // let=5
+    // console.log(typeof typeof 5);
+
+    // let a=5;
+    // console.log(a===a);
+
+    //function scope
+    // function asd(){
+    //     let a=5;
+    //     console.log(a);
+    // }    
+    // asd();
+    // console.log(a);  
+    
+    //block scope
+    // {
+    //     let a=5;
+    //     console.log(a);
+    // }    
+    // console.log(a);
+    // var a=5;
+    // console.log(a);   
+    // {
+    //     var a=10;
+    //     console.log(a);
+    // }   
+    
+    
